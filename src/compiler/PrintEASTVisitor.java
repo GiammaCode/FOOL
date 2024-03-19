cf122e09af9c76cc63d9e56cb2d5c2d30512b5e8
Luca Pasini <luca.pasini8@studio.unibo.it>
2024-03-19 11:51:39 +0100

    implementig EAST visitor

--- src/compiler/PrintEASTVisitor.java
+++ src/compiler/PrintEASTVisitor.java
@@ -207,4 +207,80 @@ public class PrintEASTVisitor extends BaseEASTVisitor<Void,VoidException> {
 		return null;
 	}
 
+	// OO Implementation
+	@Override
+	public Void visitNode(ClassNode n) {
+		printNode(n, "Class id: " + n.id);
+		for (Node field : n.fieldList) visit(field);
+		for (Node method : n.methodNode) visit(method);
+		return null;
+	}
+
+	@Override
+	public Void visitNode(FieldNode n) {
+		printNode(n, "Field id: " + n.id);
+		return null;
+	}
+
+	@Override
+	public Void visitNode(MethodNode n) {
+		//uguale a funNode
+		printNode(n, "Method id: " + n.id);
+		visit(n.retType);
+		for (ParNode par : n.parlist) visit(par);
+		for (DecNode dec : n.declist) visit(dec);
+		visit(n.exp);
+		return null;
+	}
+
+	@Override
+	public Void visitNode(ClassCallNode n) {
+		printNode(n, "Class id: " + n.classID.id + " Method id " + n.methodID +  " at nesting level: " + n.nestingLevel  );
+		visit(n.classID); //stampo il tipo della classe
+		visit(n.stEntry); //printo la entry della dichirazione della classe
+		visit(n.methodStentry); //printo la entry della dichirazione del metodo
+		for(Node node : n.listNode) visit(node);
+		return null;
+	}
+
+	@Override
+	public Void visitNode(NewNode n) {
+		printNode(n, "New node with id: " + n.id);
+		for (Node arg : n.argList) visit(arg);
+		visit(n.stentry);//visitiamo l'stEntry del nuovo nodo
+		return null;
+	}
+
+	@Override
+	public Void visitNode(EmptyNode n) {
+		printNode(n,"Empty node");
+		return null;
+	}
+
+	@Override
+	public Void visitNode(ClassTypeNode n) {
+		printNode(n);
+		for (TypeNode fieldType : n.allFields) visit(fieldType);
+		for (ArrowTypeNode methodType : n.allMethods) visit(methodType);
+		return null;
+	}
+
+	@Override
+	public Void visitNode(MethodTypeNode n) {
+		printNode(n);
+		visit(n.fun);
+		return null;
+	}
+
+	@Override
+	public Void visitNode(RefTypeNode n) {
+		printNode(n, "Class refId: " + n.id);
+		return null;
+	}
+
+	@Override
+	public Void visitNode(EmptyTypeNode n) {
+		printNode(n);
+		return null;
+	}
 }
