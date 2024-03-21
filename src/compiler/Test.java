bb3b705e0b7539d70df66fd29a04a0296484118b
Gianmaria Casamenti <gianmaria.casamenti@studio.unibo.it>
2024-03-21 19:17:46 +0100

    add new test, le classi vanno ma non funziona il test QuickSOart

--- src/compiler/Test.java
+++ src/compiler/Test.java
@@ -9,8 +9,9 @@ import svm.*;
 
 public class Test {
     public static void main(String[] args) throws Exception {
-   			
-    	String fileName = "test/Quicksort_test2.fool";
+
+		String fileName = "test/provaBase.fool";
+    	//String fileName = "test/Quicksort_test2.fool";
     	CharStream chars = CharStreams.fromFileName(fileName);
 		/*
 		 * Il lexer data una sequenza di caratteri, li aggrega in token di opportune categorie
